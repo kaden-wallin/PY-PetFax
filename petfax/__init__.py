06c1ea8c4ea3d434d0402157dc7ba4fae914aab4
BuildTools <unconfigured@null.spigotmc.org>
2023-03-31 23:34:38 -0600

    did the project

---
+++ petfax/__init__.py
@@ -0,0 +1,19 @@
+#  imports
+from flask import Flask
+
+# factory function
+def create_app():
+    app = Flask(__name__)
+
+    @app.route('/')
+    def index():
+        return 'Hello, PetFax!'
+    
+    # register blueprints
+    from . import pet
+    app.register_blueprint(pet.bp)
+
+    from . import fact
+    app.register_blueprint(fact.bp)
+    
+    return app
---
+++ petfax/fact.py
@@ -0,0 +1,11 @@
+from flask import ( Blueprint, render_template ) 
+
+bp = Blueprint(
+    'fact',
+    __name__,
+    url_prefix="/facts"
+)
+
+@bp.route('/new')
+def new(): 
+    return render_template('new_facts.html')
